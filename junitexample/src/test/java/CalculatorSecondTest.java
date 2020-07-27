6a2928240e28a96a445aefde519a5a257a71c296
seb ost <sebastian.ostrowski@gmail.com>
2020-07-27 13:16:14 -0700

    test case Id for java

--- junitexample/src/test/java/CalculatorSecondTest.java
+++ junitexample/src/test/java/CalculatorSecondTest.java
@@ -3,7 +3,7 @@ import org.junit.Test;
 public class CalculatorSecondTest {
 
     @Test
-    public void addingTest(){
+    public void addingTest_LOMA0006(){
         Calculator calc =  new Calculator();
         assert calc.add(1,1) == 2;
     }
